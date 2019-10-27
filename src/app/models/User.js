709086a3162ac54e9f20ad1225473267ac807852
Diego Rech <diegorech7@gmail.com>
2019-10-27 16:00:00 -0300

    Add bcryptjs

---
+++ src/app/models/User.js
@@ -0,0 +1,32 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    // primeiro parâmetro do super.init será as colunas que o usuário vai preencher
+    // no BD, o segundo é um objetco com sequelize e mais algumas configs opcionais
+
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+}
+
+export default User;
