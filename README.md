400fd690d800c6ec1be61900f8278f8345fe7e84
Diego Rech <diegorech7@gmail.com>
2019-12-15 16:19:32 -0300

    Appointment date validations

--- README.md
+++ README.md
@@ -25,3 +25,10 @@ Notes:
 
 * module path from node
 
+
+
+date-fns
+
+  const hourStart = startOfHour(parseISO(date));
+    -> parseISO recebe a string gerada na req quando feito um Appointment e transforma num obeto Date do JS
+    ->startOfHour zera os minutos e segundos e guarda apenas as horas do agendamento, a idéia é poder criar apenas 1 agendamento a cada hora.
--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -22,14 +23,37 @@ class AppointmentController {
     });
 
     if (!checkIsProvider) {
-      return (
-        res
-          // 401 - não autorizado
-          .status(401)
-          .json({ error: 'You can only create appointments with providers' })
-      );
+      return res
+        .status(401)
+        .json({ error: 'You can only create appointments with providers' });
     }
 
+    // Check for past dates
+    // Checa se a data já não passou
+    const hourStart = startOfHour(parseISO(date));
+    // hourStart guarda a hora passada no req em formato de objeto Date do JS
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    // Check date availability
+    // Checa disponibilidade da data
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        deleted_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    // Create the appointment
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
